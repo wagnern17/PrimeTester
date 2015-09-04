69b7e5d64770f7f4d06f42f96da8dc251d654916
wagnern17 <wagnern17@up.edu>
2015-09-03 17:07:57 -0700

    Fixed Prime # Calc

--- app/src/main/java/edu/up/cs371/primetester/PrimeTester.java
+++ app/src/main/java/edu/up/cs371/primetester/PrimeTester.java
@@ -14,7 +14,14 @@ public class PrimeTester {
      * @return true iff n is prime
      */
     public static boolean isPrime(long n) {
-        // for now, return a random result
-        return Math.random() > 0.5;
+        if (n==0||n==1){
+            return false;
+        }
+        for (int i = 2;i<n;i++){
+            if (n%i==0){
+                return false;
+            }
+        }
+        return true;
     }
 }
